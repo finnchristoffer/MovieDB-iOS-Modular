9f9cc4780cf21bd19d9350ccbd1a84a676fef245
finnchristoffer <finn.christoffer@gmail.com>
2023-05-13 20:50:11 +0700

    Initial commit

---
+++ MovieDB Modular/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDB Modular
+//
+//  Created by Finn Christoffer Kurniawan on 13/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
